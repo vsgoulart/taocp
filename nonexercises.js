cf26188b66332c201b37e0b41293fb577b6ed87c
Vinícius Goulart <goulart.vs@gmail.com>
2016-11-23 17:41:38 -0200

    Adds gcd() to nonexercises.js

--- nonexercises.js
+++ nonexercises.js
@@ -1,4 +1,34 @@
 "use strict"
 
-var a = 1;
-console.log(a);
+const nonexercises = {
+	gcd: function(m, n) {
+		if(typeof m !== "number" || typeof n !== "number")
+			throw new TypeError("Parameters must be numbers.");
+
+		if(!Number.isInteger(m) || !Number.isInteger(n))
+			throw new TypeError("Parameters must be two integer values.");
+
+		if(m <= 0 || n <= 0)
+			throw new RangeError("Parameters must be greater than zero.");
+
+		if(n > m) {
+			var m_holder = m;
+			
+			m = n;
+			n = m_holder;
+		}
+
+		var r = -1;
+
+		do {
+			r = m % n;
+			m = n;
+			if(r != 0)
+				n = r;
+		} while(r != 0);
+
+		return n;
+	}
+}
+
+module.exports = nonexercises;
